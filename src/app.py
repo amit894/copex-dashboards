fadf14afe71a7f094bd9b0fdfc60983db275e066
Amit Raj <amit.894@gmail.com>
2022-01-17 23:06:43 +0530

    Basic Python Client

--- src/app.py
+++ src/app.py
@@ -1,14 +1,4 @@
-import os
+from google.cloud import bigquery
+from google.oauth2 import service_account
 
-class App():
-    def __init__(self):
-        pass
-
-    @staticmethod
-    def run_command(cmd,args):
-        result_code=os.system(cmd+" "+args)
-        return (result_code)
-    @staticmethod
-    def run_command_output(cmd,args):
-        result=os.popen(cmd+" "+args).read()
-        return (result)
+client = bigquery.Client()
