78d2ed928592255975b5b8155abed3f2e1205df8
Amit Raj <amit.894@gmail.com>
2022-01-19 20:04:06 +0530

    List all Projects in GCP account

--- src/app.py
+++ src/app.py
@@ -2,3 +2,6 @@ from google.cloud import bigquery
 from google.oauth2 import service_account
 
 client = bigquery.Client()
+
+for project in client.list_projects():
+        print(project.project_id)
